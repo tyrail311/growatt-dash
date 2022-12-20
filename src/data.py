bfffc0936cb700cfd37ded27fb2393a781232384
tpnadeau <tyrail311@gmail.com>
2022-12-20 16:00:45 -0500

    get inverter data

--- src/data.py
+++ src/data.py
@@ -5,8 +5,15 @@ gw = growattServer.GrowattApi()
 gw.server_url='https://server-us.growatt.com/'
 login_response = gw.login('patrick.h.nadeau', 'mitvug-xurdoz-8coZdy')
 plant = gw.plant_list(login_response['user']['id'])
-print(plant)
 plant_id = plant['data'][0]['plantId']
-pprint(plant_id)
 plant_data = gw.plant_info(plant_id)
-pprint(plant_data)
+inverters = gw.inverter_list(plant_id)
+inverter_sns = []
+
+for sn in range(0, 4):
+    inverter_sn = inverters[sn]['deviceSn']
+    inverter_sns.append(str(inverter_sn))
+    
+for sn in inverter_sns:
+    inverter_data = gw.inverter_detail(sn)
+    pprint(inverter_data)
